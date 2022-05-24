9cbebc10b776ed96f90dbd85ee6dd519cc4814ee
kimdaehun <dae9341@gmail.com>
2022-05-24 17:27:20 +0900

    이미지추출 js수정

--- playwright/imgExtract.js
+++ playwright/imgExtract.js
@@ -1,14 +1,56 @@
 const {chromium,devices} = require('playwright');
 const iPhone = devices['iPhone 6'];
+const fs = require('fs');
 
+// TODO: 중복제거, 이미지url포맷변경, 안보이는 이미지 해결책 
 (async()=>{
-    const browser = await chromium.launch();
-    const context = await browser.newContext()
-    const page = await browser.newPage();
-    await page.goto('https://www.gongyoungshop.kr/event/selectEventDetail.do?eventNo=1160');  
-    await page.on('domcontentloaded', async()=>{
-        // await page.screenshot({path:'event.png',fullPage:true});
-    })();
+    const browser = await chromium.launch({
+        headless:true
+    });
+    const context = await browser.newContext({
+        ...iPhone
+    });
+    const page = await context.newPage();
+    await page.goto('https://sm.gongyoungshop.kr/event/selectEventDetail.do?eventNo=1206');
+    var srcs = await page.evaluate(function(){
+        let srcList = [];
+        $('.eventContentWrap img').each(function(){
+            let $this = $(this);
+            srcList.push($this.attr('src').split('img.publichs.com')[1]);
+        });
+        return srcList;
+    })
 
-    await browser.close();
+
+    var bgs = await page.evaluate(function(){
+        let bgList = [];
+
+        function getChildrenEl(wrapperId){
+            let $wrapperId = $(wrapperId);
+            if($(wrapperId).children().length){
+                $(wrapperId).children().each(function(){
+                    if($(this).css("background-image")=='none') return;
+                    let imgUrl = $(this).css("background-image").replace('url("','').replace('")','');
+                    bgList.push(imgUrl.split('img.publichs.com')[1]);
+                    getChildrenEl($(this));
+                })
+            }
+        }
+
+        getChildrenEl($('.eventContentWrap'));
+
+        return bgList;
+    })
+
+    srcs = srcs.concat(bgs);
+
+    await fs.writeFile('imgList.json',JSON.stringify(srcs),function(err){
+        if(err) throw err;
+        console.log('[fs] write json');
+    });
+    
+
+
+    await browser.close()
+    
 })();
